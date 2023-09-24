3b50ba952ccb5e6719614512873b16eb36b4c85e
tmokmss <tmokmss@users.noreply.github.com>
2023-09-24 12:28:07 +0900

    rename

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -7,6 +7,8 @@ import path = require('path');
 import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
 import { NodejsBuild } from 'deploy-time-build';
 import { readFileSync } from 'fs';
+import { App } from '@aws-cdk/aws-amplify-alpha';
+import { execSync } from 'child_process';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -32,6 +34,65 @@ export class CdkStack extends cdk.Stack {
 
     new cdk.CfnOutput(this, 'UserPoolId', { value: userPool.userPoolId });
     new cdk.CfnOutput(this, 'UserPoolClientId', { value: client.userPoolClientId });
+
+    {
+      const parent = new Construct(this, 'LocalBuild');
+      const assetBucket = new Bucket(parent, 'AssetBucket', {
+        encryption: BucketEncryption.S3_MANAGED,
+        blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+        enforceSSL: true,
+        removalPolicy: cdk.RemovalPolicy.DESTROY,
+        autoDeleteObjects: true,
+      });
+
+      const originAccessIdentity = new OriginAccessIdentity(parent, 'OriginAccessIdentity');
+      const distribution = new CloudFrontWebDistribution(parent, 'Distribution', {
+        originConfigs: [
+          {
+            s3OriginSource: {
+              s3BucketSource: assetBucket,
+              originAccessIdentity,
+            },
+            behaviors: [
+              {
+                isDefaultBehavior: true,
+              },
+            ],
+          },
+        ],
+        errorConfigurations: [
+          {
+            errorCode: 404,
+            errorCachingMinTtl: 0,
+            responseCode: 200,
+            responsePagePath: '/',
+          },
+          {
+            errorCode: 403,
+            errorCachingMinTtl: 0,
+            responseCode: 200,
+            responsePagePath: '/',
+          },
+        ],
+      });
+
+      new cdk.CfnOutput(parent, 'FrontendUrl', { value: `https://${distribution.distributionDomainName}` });
+
+      const deployment = new BucketDeployment(parent, 'DeployWebsite', {
+        sources: [
+          Source.asset(path.join('..', 'frontend'), {
+            bundling: {
+              image: cdk.DockerImage.fromRegistry('node:18'),
+              command: ['sh', '-c', `npm install && npm run build && cp -r out/. /asset-output`],
+              user: 'root',
+            },
+          }),
+        ],
+        destinationBucket: assetBucket,
+        destinationKeyPrefix: '/',
+        distribution,
+      });
+    }
 
     {
       const parent = new Construct(this, 'RuntimeConfig');
@@ -137,8 +198,8 @@ export class CdkStack extends cdk.Stack {
       const deployment = new NodejsBuild(parent, 'DeployWebsite', {
         assets: [
           {
-            path: path.join('..', 'frontend-deploy-time-build'),
-            exclude: readFileSync(path.join('..', 'frontend-deploy-time-build', '.gitignore')).toString().split('\n'),
+            path: path.join('..', 'frontend'),
+            exclude: readFileSync(path.join('..', 'frontend', '.gitignore')).toString().split('\n'),
             ignoreMode: cdk.IgnoreMode.GIT,
           },
         ],
@@ -153,5 +214,29 @@ export class CdkStack extends cdk.Stack {
         },
       });
     }
+    // {
+    //   const amplifyApp = new App(this, 'MyApp', {});
+    //   const branch = amplifyApp.addBranch('dev', { asset: asset });
+    // }
+    const arn = 'arn:aws:apprunner:us-east-1:123456789012:service/SERVICE_NAME/SERVICE_ID';
+    /**
+     * Cloudformaton does not return the serviceName attribute so we extract it from the serviceArn.
+     * The ARN comes with this format:
+     * arn:aws:apprunner:us-east-1:123456789012:service/SERVICE_NAME/SERVICE_ID
+     */
+    /*
+const sv = new apprunner.CfnService(this, 'Service', {
+  ...
+});
+// The format of attrServiceArn: 
+//   "arn:aws:apprunner:us-east-1:123456789012:service/SERVICE_NAME/SERVICE_ID"
+// You CANNOT reference the value on synthesis time
+const resourceFullName = sv.attrServiceArn.split(":")[5]; // this does NOT work!
+console.log(sv.attrServiceArn) // "${TOKEN[1234]}"
+// In the CFn template this will be resolved to: 
+//   { "Fn::GetAtt" : [ "Service" , "ServiceArn" ] }
+// To interact with the value on deploy time, use CFn intrinsic functions
+const resourceFullName = cdk.Fn.select(5, cdk.Fn.split(':', sv.attrServiceArn));
+    */
   }
 }
